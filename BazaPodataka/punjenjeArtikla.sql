20865c3205423341865bebd12c730685d98995a6
Niko Torić <35310496+Nikotoric@users.noreply.github.com>
2020-09-09 10:49:43 +0200

    Add files via upload

---
+++ BazaPodataka/punjenjeArtikla.sql
@@ -0,0 +1,53 @@
+SET IDENTITY_INSERT [dbo].[Artikli] ON
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (1, N'Pizza miješana', CAST(35.00 AS Decimal(18, 2)), 0)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (2, N'Pizza margarita', CAST(35.00 AS Decimal(18, 2)), 0)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (3, N'Pizza vesuvio', CAST(33.00 AS Decimal(18, 2)), 0)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (4, N'Pizza funghi', CAST(33.00 AS Decimal(18, 2)), 0)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (5, N'Pizza tunjevina', CAST(36.00 AS Decimal(18, 2)), 0)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (6, N'Pizza 4 vrste sira', CAST(36.00 AS Decimal(18, 2)), 0)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (7, N'Pizza slavonska', CAST(38.00 AS Decimal(18, 2)), 0)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (8, N'Pizza plodovi mora', CAST(38.00 AS Decimal(18, 2)), 0)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (9, N'Pizza mexicana', CAST(38.00 AS Decimal(18, 2)), 0)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (10, N'Pizza vegetariana', CAST(32.00 AS Decimal(18, 2)), 0)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (11, N'Sendviè: Šunka - sir', CAST(15.00 AS Decimal(18, 2)), 1)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (12, N'Sendviè: Pršut - sir', CAST(15.00 AS Decimal(18, 2)), 1)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (13, N'Sendviè: Kulen - sir', CAST(15.00 AS Decimal(18, 2)), 1)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (14, N'Sendviè: Tuna - sir', CAST(15.00 AS Decimal(18, 2)), 1)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (15, N'Piletina', CAST(22.00 AS Decimal(18, 2)), 1)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (16, N'Piletina sa sirom', CAST(25.00 AS Decimal(18, 2)), 1)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (17, N'Hamburger', CAST(22.00 AS Decimal(18, 2)), 1)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (18, N'Cheeseburger', CAST(25.00 AS Decimal(18, 2)), 1)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (19, N'Voda', CAST(10.00 AS Decimal(18, 2)), 2)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (20, N'Ledeni èaj', CAST(12.99 AS Decimal(18, 2)), 2)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (21, N'Coca Cola', CAST(14.99 AS Decimal(18, 2)), 2)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (22, N'Sprite', CAST(14.99 AS Decimal(18, 2)), 2)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (23, N'Fanta', CAST(14.99 AS Decimal(18, 2)), 2)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (24, N'Cedevita', CAST(14.99 AS Decimal(18, 2)), 2)
+GO
+INSERT [dbo].[Artikli] ([ArtikalID], [imeArtikla], [cijena], [tipArtikla]) VALUES (25, N'Pivo 0.5L', CAST(14.99 AS Decimal(18, 2)), 2)
+GO
+SET IDENTITY_INSERT [dbo].[Artikli] OFF
